99f2a7a34751e2555a636c32f416bb7f913f1749
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-05-07 13:55:57 +0600

    updating delivered api

--- indexxx.js
+++ indexxx.js
@@ -1,36 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
-
 //middleware
 app.use(cors());
 app.use(express.json());
-
 // database connection 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ham2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
+// client.connect(err => {
+//   const collection = client.db("CuteCar").collection("cars");
+//   // perform actions on the collection object
+//   console.log('Mongo is connected');
+//   client.close();
+// });
 // Getting all card 
-
 async function run(){
     try{
         await client.connect();
         const carCollection = client.db('cuteCar').collection('cars');
-
         app.get('/cars',async(req,res)=>{
             const query = {};
             const cursor = carCollection.find(query);
             const cars = await cursor.toArray();
             res.send(cars);
         });
-
         // Getting sigle cars 
         app.get('/cars/:id', async(req, res) =>{
             const id = req.params.id;
@@ -38,13 +34,13 @@ async function run(){
             const cars = await carCollection.findOne(query);
             res.send(cars);
         });
-
          // POST api or adding a new car
          app.post('/cars', async(req, res) =>{
             const newcars = req.body;
             const result = await carCollection.insertOne(newcars);
             res.send(result);
         });
+
 
          // DELETE or removing a car
          app.delete('/cars/:id', async(req, res) =>{
@@ -54,53 +50,14 @@ async function run(){
             res.send(result);
         });
 
-          // update cars
-          app.put('/cars/:id', async(req, res) =>{
-            const id = req.params.id;
-            const updatedCars = req.body;
-            const filter = {_id: ObjectId(id)};
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    quantity: updatedCars.quantity,
-                    
-                }
-            };
-            const result = await carCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-
-        })
-
-         // update quantity and sold
-         app.put('/cars/:id', async(req, res) =>{
-            const id = req.params.id;
-            const updatedCars = req.body;
-            const filter = {_id: ObjectId(id)};
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    quantity: updatedCars.quantity,
-                    sold: updatedCars.sold,
-                    
-                }
-            };
-            const result = await carCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-
-        })
-
     }
     finally{
-
     }
 }
 run().catch(console.dir);
-
-
 app.get('/',(req,res)=>{
     res.send('Cute server is running')
 });
-
 app.listen(port, ()=>{
     console.log('Cute car running on port 5000');
 })
