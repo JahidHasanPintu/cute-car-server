99b345b53d797c27507c2822f22cf209a6a6fba8
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-05-08 08:40:05 +0600

    access tverification

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//token verification
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 // database connection 
 
 
